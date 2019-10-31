e0e246c52e56b3f410ae3ff2accbaf74289597eb
RonaldRamoss <ronaldvalentimramos@gmail.com>
2019-10-31 19:05:12 +0900

    Project's Initial Commit

---
+++ www/components/editar.js
@@ -0,0 +1,33 @@
+$(document).on("click","#enviar",function(){
+
+var parametros ={
+"nome":$("nome").val(),
+"modelo":$("modelo").val(),
+"marca":$("marca").val(),
+"tipo":$("tipo").val(),
+"placa":$("placa").val(),
+"entrada":$("entrada").val()
+};
+
+$.ajax({
+type: "post",
+url: "www",
+data: parametros,
+success: function(data){
+  navigator.notification.alert(data);
+  $("nome").val("");
+  $("modelo").val("");
+  $("marca").val("");
+  $("tipo").val("");
+  $("placa").val("");
+  $("entrada").val("");
+},
+error: function(data){
+  navigator.notification.alert("Erro ao cadastrar");
+}
+
+
+
+})
+});
+
